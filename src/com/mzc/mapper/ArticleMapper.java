0b6781bde7aa7aabe52e9f023902df69a1fb631e
mzc <2840679020@qq.com>
2019-10-15 18:34:38 +0800

    反程序大牛

---
+++ src/com/mzc/mapper/ArticleMapper.java
@@ -0,0 +1,20 @@
+package com.mzc.mapper;
+
+import com.mzc.bean.Article;
+import com.mzc.bean.ArticleWithBLOBs;
+
+public interface ArticleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ArticleWithBLOBs record);
+
+    int insertSelective(ArticleWithBLOBs record);
+
+    ArticleWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ArticleWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(ArticleWithBLOBs record);
+
+    int updateByPrimaryKey(Article record);
+}
---
+++ src/com/mzc/mapper/ChannelMapper.java
@@ -0,0 +1,17 @@
+package com.mzc.mapper;
+
+import com.mzc.bean.Channel;
+
+public interface ChannelMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Channel record);
+
+    int insertSelective(Channel record);
+
+    Channel selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Channel record);
+
+    int updateByPrimaryKey(Channel record);
+}
